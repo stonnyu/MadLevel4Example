31eb8d2fd738e6786f2ffae305f2eb2ced14a009
Tonny <mrtonnysu@gmail.com>
2020-10-18 17:19:36 +0200

    Setting up database

---
+++ app/src/main/java/com/example/madlevel4example/ReminderDao.kt
@@ -0,0 +1,21 @@
+package com.example.madlevel4example
+
+import Reminder
+import androidx.room.*
+
+@Dao
+interface ReminderDao {
+
+    @Query("SELECT * FROM reminderTable")
+    fun getAllReminders(): List<Reminder>
+
+    @Insert
+    fun insertReminder(reminder: Reminder)
+
+    @Delete
+    fun deleteReminder(reminder: Reminder)
+
+    @Update
+    fun updateReminder(reminder: Reminder)
+
+}
---
+++ app/src/main/java/com/example/madlevel4example/ReminderRepository.kt
@@ -0,0 +1,31 @@
+package com.example.madlevel4example
+
+import Reminder
+import android.content.Context
+
+class ReminderRepository(context: Context) {
+
+    private var reminderDao: ReminderDao
+
+    init {
+        val reminderRoomDatabase = ReminderRoomDatabase.getDatabase(context)
+        reminderDao = reminderRoomDatabase!!.reminderDao()
+    }
+
+    fun getAllReminders(): List<Reminder> {
+        return reminderDao.getAllReminders()
+    }
+
+    fun insertReminder(reminder: Reminder) {
+        reminderDao.insertReminder(reminder)
+    }
+
+    fun deleteReminder(reminder: Reminder) {
+        reminderDao.deleteReminder(reminder)
+    }
+
+    fun updateReminder(reminder: Reminder) {
+        reminderDao.updateReminder(reminder)
+    }
+
+}
